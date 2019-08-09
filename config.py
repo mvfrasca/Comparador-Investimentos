ee1f12ad6f00bd2c965577fe62aa96d6afe57558
Marcelo Valverde Frasca <mvfrasca@gmail.com>
2019-08-09 00:28:58 -0300

    Ajustes configurações

--- config.py
+++ config.py
@@ -32,7 +32,8 @@ DATA_BACKEND = 'datastore'
 
 # Google Cloud Project ID. This can be found on the 'Overview' page at
 # https://console.developers.google.com
-PROJECT_ID = 'Comparador-Investimentos'
+# PROJECT_ID = 'Comparador-Investimentos'
+PROJECT_ID = 'compinvest-backend'
 
 # CloudSQL & SQLAlchemy configuration
 # Replace the following values the respective values of your Cloud SQL
